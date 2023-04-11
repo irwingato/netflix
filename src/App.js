92776f3e075901e9842226cbdd4fbad3920d9eda
Irwing Seiji Ato <atoseiji@gmail.com>
2023-04-11 14:28:36 -0300

    criando estrutura do app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Row from "./components/Row"
+import { categories } from './api';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+
+    return (
+      <div className="App">
+        {/* Navbar */}
+        {/* destaque */}
+        {/* Em alta */}
+        {categories.map ( (category) => {
+          return <Row key={category.name} title={category.title} path={category.path} />
+        } )}
+
+      </div>
+    );
+  }
 
 export default App;
